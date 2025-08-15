cf7324c23fd6f1e12f5b4f3970ef70e0997b7bdf
thediaax <gabrieldias23.dev@gmail.com>
2025-08-15 00:01:38 -0300

    fix: adiciona arquivos corretos ao projeto springboot

---
+++ meal4you-api/src/test/java/com/meal4you/meal4you_api/Meal4youApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meal4you.meal4you_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Meal4youApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
